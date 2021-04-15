43cfe5bccc3b902b56026b5d1640127737eeda18
patrick.siegel <patrick.siegel@senacor.com>
2021-04-15 19:00:27 +0200

    Add some unit tests

---
+++ src/app/core/items.adapter.spec.ts
@@ -0,0 +1,51 @@
+import {TestBed} from '@angular/core/testing';
+import {ItemsAdapter} from "./items.adapter";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {AppConfig} from "./model/app-config.model";
+import {Item} from "./model/item.model";
+
+describe('ItemsAdapter', () => {
+  let subject: ItemsAdapter;
+  let httpMock: HttpTestingController;
+
+  const windowMock = {
+    config: {
+      endpoints: {
+        items: 'http://host:1234/items'
+      }
+    } as AppConfig
+  }
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [{provide: 'Window', useValue: windowMock}],
+      imports: [HttpClientTestingModule]
+    });
+    subject = TestBed.inject(ItemsAdapter)
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  it('should be created', () => {
+    expect(subject).toBeTruthy()
+  });
+
+  it('#get() works w/ name param', () => {
+    const res = [{name: 'test', available: 1, price: 1.99} as Item];
+    subject.get('test').subscribe((n) => expect(n).toEqual(res));
+
+    const req = httpMock.expectOne('http://host:1234/items?name=test');
+    expect(req.request.method).toBe('GET');
+    req.flush(res);
+    httpMock.verify();
+  });
+
+  it('#get() works w/o name param', () => {
+    const res = [{name: 'test', available: 1, price: 1.99} as Item];
+    subject.get().subscribe((n) => expect(n).toEqual(res));
+
+    const req = httpMock.expectOne('http://host:1234/items');
+    expect(req.request.method).toBe('GET');
+    req.flush(res);
+    httpMock.verify();
+  });
+});
--- src/app/search/search.component.spec.ts
+++ src/app/search/search.component.spec.ts
@@ -1,25 +1,50 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {SearchComponent} from './search.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {ItemsAccess, ItemsAdapter} from "../core/items.adapter";
+import {of} from "rxjs";
+import {Item} from "../core/model/item.model";
 
 describe('SearchComponent', () => {
-  let component: SearchComponent;
+  let subject: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
+  let adapterSpy: jasmine.Spy;
+
+  const items = (name: string| null) => [{name: name || 'null', available: 1, price: 1.99} as Item];
+  const itemsAdapterMock: ItemsAccess = {
+    get: (name: string | null) => {
+      return of(items(name))
+    }
+  }
 
   beforeEach(async () => {
+    adapterSpy = spyOn(itemsAdapterMock, 'get').and.callThrough();
     await TestBed.configureTestingModule({
-      declarations: [ SearchComponent ]
+      declarations: [ SearchComponent ],
+      providers: [{provide: ItemsAdapter, useValue: itemsAdapterMock}],
+      imports: [ReactiveFormsModule]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SearchComponent);
-    component = fixture.componentInstance;
+    subject = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(subject).toBeTruthy();
+    expect(subject.form.controls.name.value).toEqual('')
+  });
+
+  it('#onSearch() triggers adapter method', () => {
+    subject.form.controls.name.setValue('test');
+
+    subject.onSearch();
+
+    expect(adapterSpy).toHaveBeenCalledOnceWith('test');
+    expect(subject.searchResult).toEqual(items('test'));
   });
 });
