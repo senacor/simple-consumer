5b3d88e4166f288e1309097be85f90b7233b8506
pschulz <patrick.leon.schulz@gmail.com>
2021-05-31 16:07:25 +0200

    fix entrypoint

--- runtime/launch.sh
+++ runtime/launch.sh
@@ -4,7 +4,8 @@ export "TZ=Europe/Berlin"
 # Log environment
 export
 
-envsubst < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html
+cp /usr/share/nginx/html/index.html /usr/share/nginx/html/index_template.html
+cat /usr/share/nginx/html/index_template.html | envsubst > /usr/share/nginx/html/index.html
 
 # Start nginx actually:
-exec nginx -c /etc/nginx/nginx.conf
+exec nginx -g 'daemon off;' -c /etc/nginx/nginx.conf
